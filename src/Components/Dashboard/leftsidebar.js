6011ed29ed4d9df8052776e7e1dd27b34980c659
Lukman10a <abdulrauflukman9@gmail.com>
2023-04-24 15:21:42 +0100

    add more sidebar items

--- src/Components/Dashboard/leftsidebar.js
+++ src/Components/Dashboard/leftsidebar.js
@@ -4,11 +4,14 @@ import mainstack from "../../assets/icons/mainstack-logo.svg";
 import edit from "../../assets/icons/edit.svg";
 import group from "../../assets/icons/group.svg";
 import hourglass from "../../assets/icons/hourglass_empty.svg";
+import addphoto from "../../assets/icons/add_a_photo.svg";
+import deletes from "../../assets/icons/delete.svg";
 
 const LeftSideBar = () => {
   return (
     <div className=" border-r-2">
       <img src={mainstack} alt="" className="my-7 px-7" />
+
       <ul className="space-y-4">
         <li className="flex gap-4 text-[#FF5403] border-[#FF5403] px-7 border-l-2">
           <img src={dashboard} alt="" />
@@ -26,10 +29,24 @@ const LeftSideBar = () => {
         </li>
 
         <li className="px-7 flex gap-4">
-          <img src={hourglass} alt="" />
+          <img src={deletes} alt="" />
           <p>Item 3</p>
         </li>
       </ul>
+
+      <p className="my-7 px-7">OTHERS 1</p>
+<ul className="space-y-4">
+
+      <li className="px-7 flex gap-4">
+        <img src={addphoto} alt="" />
+        <p>Item 4</p>
+      </li>
+
+      <li className="px-7 flex gap-4">
+        <img src={hourglass} alt="" />
+        <p>Item 5</p>
+      </li>
+</ul>
     </div>
   );
 };
