d4a50dc29c7a434f4fe9f87d5494048a60832495
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2023-04-24 21:35:47 +0100

    feat: finish integrating api for views graph

--- src/Components/Dashboard/leftsidebar.js
+++ src/Components/Dashboard/leftsidebar.js
@@ -6,6 +6,9 @@ import group from "../../assets/icons/group.svg";
 import hourglass from "../../assets/icons/hourglass_empty.svg";
 import addphoto from "../../assets/icons/add_a_photo.svg";
 import deletes from "../../assets/icons/delete.svg";
+import subscriptions from "../../assets/icons/subscriptions.svg";
+import file from "../../assets/icons/file_present.svg";
+import alarm from "../../assets/icons/alarm.svg";
 
 const LeftSideBar = () => {
   return (
@@ -13,40 +16,65 @@ const LeftSideBar = () => {
       <img src={mainstack} alt="" className="my-7 px-7" />
 
       <ul className="space-y-4">
-        <li className="flex gap-4 text-[#FF5403] border-[#FF5403] px-7 border-l-2">
+        <li className="flex gap-4  text-[#FF5403] border-[#FF5403] px-7 border-l-2">
           <img src={dashboard} alt="" />
           <p>Dashboard</p>
         </li>
 
         <li className="px-7 flex gap-4">
           <img src={edit} alt="" />
-          <p>Item 1</p>
+          <p className="text-[#56616B]">Item 1</p>
         </li>
 
         <li className="px-7 flex gap-4">
           <img src={group} alt="" />
-          <p>Item 2</p>
+          <p className="text-[#56616B]">Item 2</p>
         </li>
 
         <li className="px-7 flex gap-4">
           <img src={deletes} alt="" />
-          <p>Item 3</p>
+          <p className="text-[#56616B]">Item 3</p>
         </li>
       </ul>
+      <div>
+        <p className="my-7 px-7 text-[#4D5760]">OTHERS 1</p>
+        <ul className="space-y-4">
+          <li className="px-7 flex gap-4">
+            <img src={addphoto} alt="" />
+            <p className="text-[#56616B]">Item 4</p>
+          </li>
 
-      <p className="my-7 px-7">OTHERS 1</p>
-<ul className="space-y-4">
+          <li className="px-7 flex gap-4">
+            <img src={hourglass} alt="" />
+            <p className="text-[#56616B] font-medium">Item 5</p>
+          </li>
+        </ul>
+      </div>
 
-      <li className="px-7 flex gap-4">
-        <img src={addphoto} alt="" />
-        <p>Item 4</p>
-      </li>
+      <div>
+        <p className="my-7 px-7 text-[#4D5760]">OTHERS 2</p>
+        <ul className="space-y-4">
+          <li className="px-7 flex gap-4">
+            <img src={subscriptions} alt="" />
+            <p className="text-[#56616B] font-medium">Item 6</p>
+          </li>
 
-      <li className="px-7 flex gap-4">
-        <img src={hourglass} alt="" />
-        <p>Item 5</p>
-      </li>
-</ul>
+          <li className="px-7 flex gap-4">
+            <img src={file} alt="" />
+            <p className="text-[#56616B] font-medium">Item 7</p>
+          </li>
+
+          <li className="px-7 flex gap-4">
+            <img src={alarm} alt="" />
+            <p className="text-[#56616B]">Item 8</p>
+          </li>
+        </ul>
+
+        <div>
+          <img src="" alt="" />
+          <p>Blessing Daniels</p>
+        </div>
+      </div>
     </div>
   );
 };
