89d47eaaca1bcef9d701007b843668a271a5210c
Lukman10a <abdulrauflukman9@gmail.com>
2023-04-24 15:04:26 +0100

    create basic structure

---
+++ src/Components/Dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LeftSideBar from "./leftsidebar";
+import MainContent from "./maincontent";
+
+const DashBoard = () => {
+  return (
+    <div className="flex h-full ">
+      <LeftSideBar />
+      <MainContent />
+    </div>
+  );
+};
+
+export default DashBoard;
---
+++ src/Components/Dashboard/leftsidebar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import dashboard from "../../assets/icons/dashboard.svg";
+import mainstack from "../../assets/icons/mainstack-logo.svg";
+import edit from "../../assets/icons/edit.svg";
+import group from "../../assets/icons/group.svg";
+import hourglass from "../../assets/icons/hourglass_empty.svg";
+
+const LeftSideBar = () => {
+  return (
+    <div className=" border-r-2">
+      <img src={mainstack} alt="" className="my-7 px-7" />
+      <ul className="space-y-4">
+        <li className="flex gap-4 text-[#FF5403] border-[#FF5403] px-7 border-l-2">
+          <img src={dashboard} alt="" />
+          <p>Dashboard</p>
+        </li>
+
+        <li className="px-7 flex gap-4">
+          <img src={edit} alt="" />
+          <p>Item 1</p>
+        </li>
+
+        <li className="px-7 flex gap-4">
+          <img src={group} alt="" />
+          <p>Item 2</p>
+        </li>
+
+        <li className="px-7 flex gap-4">
+          <img src={hourglass} alt="" />
+          <p>Item 3</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default LeftSideBar;
